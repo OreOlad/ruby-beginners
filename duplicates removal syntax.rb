8af49c38bbbb563feb4c9aa446d1110adab6d351
OreOlad <65356045+OreOlad@users.noreply.github.com>
2020-05-26 16:31:43 -0400

    Create duplicates removal syntax.rb

---
+++ duplicates removal syntax.rb
@@ -0,0 +1,12 @@
+#uniq syntax(unintended pun)
+arr.uniq
+#union operator syntax
+arr | arr
+#intersection operator syntax
+arr & arr
+#difference operator
+arr - arr
+#and finally the to set syntax
+arr.to_set
+#to turn it back to an array
+arr.to_set.to_a
